fc045a095165f4b4f2fb444e75a3c4735921d2a5
will7hughes <will7hughes@gmail.com>
2019-11-11 14:04:24 -0600

    -1 for success 0 for fail

--- palindrome.asm
+++ palindrome.asm
@@ -1,34 +1,31 @@
 
-// if user has types something @KBD if no key is being pressed its 0
-// loop while value is 0
-// take value and put it in d register and then put in memory later
-// R0 is going to be where we put the answer to whether or not the word is a palindrome true = -1
-// false = 0
+// Assignment 6
+// Trent (Will) Hughes
+// ID: 113392078
 
-//R0 answer
-//R1 temp char Read
-//R2 Number of characters read
-//R3
-//R4
-//R5 first char Read
+//R0 Answer (-1 for true, 0 for false)
+//R1 Temp char read
+//R2 Number of chars read
+//R3 Index of last char being compared
+//R4 Offset of first char being compared
+//R5 Index of first char being compared
 //R6 2nd char read
 //R7 3rd char read
 
+// Setup offset to 1 to initialize R5
+// R5 will be values in R5 + R4 (offset)
 @R4
 D=M
 M=1
 
-
-//Making R2 -1 because later on we are incrementing before we use it
-
-//Reading if the key is zero, reread again. Loop until a key is pressed
+//Loop until key is pressed
 (START) // A=0          D=0
 @KBD    // A=KBD        D=0
 D=M     // A=KBD        D=M[KBD]
 @START  // A=(Start)    D=M[KBD]
 D;JEQ   // A=(Start)    D=M[KBD] Jump if the D is zero, GOTO @Start
 
-//Store the D register somewhere in memory
+//Store the key pressed
 @R1     // A=R1         D=M[KBD]
 M=D     // A=R1         D=M[KBD]    M[R1]=M[KBD] <-- key pressed
 
@@ -38,15 +35,17 @@ D=D-A               // A=128                    D=M[KBD]-128 <-- (key pressed - 
 @CHECK_PALINDROME   // A=(CHECK_PALINDROME)     
 D;JEQ               // A=(CHECK_PALINDROME)     D=M[KBD]-128 <-- (key pressed - 128) Jump if zero
 
-
-//Make sure the key isn't being held down
+//Check if key is still being pressed
+//Loop until released
 (START2)            
 @KBD                //A=KBD
 D=M                 //A=KBD         D=M[KBD] <-- key pressed
 @START2             //A=(START2)    D=M[KBD]
-D;JNE               //Jump if they key has been released
+D;JNE               //Jump if key released
 
-
+//Find index of memory to put the character
+//Store the char at that index
+//Increment number of characters read
 @START3             //A=(START3)    D=M[KBD]
 0;JMP              
 (START3)
@@ -62,15 +61,9 @@ D=M                 //A=R1  D=M[R1]
 A=M                 //A=M[R3] or A=M[KBD] + 6 <-- the next characters location
 M=D                 //M[KBD] + 6=M[R1] <-- We're putting temp character into it's location in memory
 
+//Return to beginning to get another character read in
 @START
 0;JMP
-
-
-// // Increment R4
-// @R4         //A=R4
-// D=M         //A=R4 D=M[R4]
-// M=D+1       //A=R4 D=M[R4] M[R4] = M[R4] + 1
-// // Increment R4
 
 (CHECK_PALINDROME)
 // Must store value of addition of M[R4] + 6 in R5
@@ -80,6 +73,8 @@ D=M             // A=R4     D=M[R4] <--- offset of first char
 M=D+A           // A=M[R6 + M[R4]]  D=M[R4] <-- (first) character 
 
 // Check if R3 = R5, end successful
+// Checks if indices are equal. 
+// If so, we have succussfully read/compared all characters
 @R3
 D=M
 @R5
@@ -88,45 +83,48 @@ D=D-A
 @SUCCESS
 D;JEQ
 
-// Setup D=D-A to check if they are zero, and we will jump if it is (w/ label)
+// Setup D register with last character for first/last char comparison (D=D-A)
 @R3             // A=R3         <-- location of last character
 D=M             // A=R3     D=M[R3]     <-- last character index (ABBA 9)
 A=D             // A=M[R3]  D=M[R3] <-- last character char
 D=M             // D[M[R3]]= M[R3]
-// the above will get last character in D is working
 
-// Get index of first character
+// Setup A register with first char
 @R5             // A=R4         <-- location of first char
 A=M             // A=R4     D=M[R4] <--- offset of first char
 A=M
 
-// Check M[KBD+1] with M[R3] or first character and last 
+// Check if last - first char equals 0
+// If it is, then it is equal. Continue... to setup next char and check next chars
+// If not, end program
 D=D-A
 @SETUP_NEXT_CHAR
 D;JEQ
 
-// Not Equal, Fail
+// Not Equal, Fail. Loop forever
+(FAIL)
+@FAIL
+0;JMP
+
+// Is palindrom, set result to -1
+// And then loop forever, doing nothing
+(SUCCESS)
 @R0
 D=M-1
 @R0
 M=D
-(FAIL)
-@33
-@FAIL
+@STOP
 0;JMP
 
-(SUCCESS)
-@77
-@SUCCESS
+(STOP)
+@STOP
 0;JMP
 
-// (CHECK_PALINDROME)
-// @CHECK_PALINDROME
-// 0;JMP
 
 (SETUP_NEXT_CHAR)
 
-// Check if R5 < R3, end successful
+// Check if R5 > R3, end successful
+// EVEN case where R5(index of first char) > R3(index of last char)
 @R3
 D=M
 @R5
@@ -135,12 +133,13 @@ D=D-A
 @SUCCESS
 D;JLE
 
-// Increment R4
+// Increment R4 (first char offset)
 @R4         //A=R4
 D=M         //A=R4 D=M[R4]
 M=D+1       //A=R4 D=M[R4] M[R4] = M[R4] + 1
 // Increment R4
 
+// Decrement R3 (last char index)
 @R3         //A=R3
 D=M         //A=R3 D=M[R3]
 M=D-1       //A=R3 D=M[R3] M[R3] = M[R3] - 1
@@ -149,113 +148,3 @@ M=D-1       //A=R3 D=M[R3] M[R3] = M[R3] - 1
 
 @SETUP_NEXT_CHAR
 0;JMP
-
-
-(TEST)
-@22
-@TEST
-0;JMP
-
-
-
-
-
-//R4 = last letter where the char is (5+R2)
-//R3 = pointer to the first letter (5)
-//subtract them @R4 D=M @R3 D=M-D D;JEQ(then increment R3 and decrement R4)
-//(Exit) @Exit 0;JMP (When you have the answer)
-//Either R3 and R4 is equal then were done, or the R3 and R4 pass each other(@R3 D=M @R4 D=D-M @IS_PALINDROME D;JGE)
-
-
-// // start at negative one
-// // @R2
-// // M=-1
-
-// (START)
-// @KBD
-// D=M
-// @START
-// D;JEQ
-
-
-// @R1 //Store keyboard code
-// M=D // In memory
-
-// (START2) // Wait for user to letup pressing the key
-// @KBD
-// D=M
-// @START2
-// D;JNE // Wait for user to stop pressing key
-
-// @START3
-// (START3)
-// @R2
-// MD=M+1
-// @5
-// D=D+A
-// @R3
-// M=D
-// @R1
-// D=M
-// @R3
-// A=M
-// M=D
-
-// @START
-// 0;JMP
-
-
-// TODO: 0;JMP to below code. We need label 
-// TODO or if they don't hit RETURN. Go somewhere else (not below code). Go back to user input code
-
-
-// @R2
-// //D=M // one operation
-// MD=M+1 // double operation (1) increment number of characters
-// @5 // Constant value
-// D=D+A // 0+5. 
-// @R3
-// M=D 
-// @R1
-// D=M
-
-// // Get R3 into permanent memory 
-// @R3
-// A=M
-// M=D
-
-// Set R3 to first
-// Set R4 to last
-
-// Do logic subtract R4-R3. if they are equal aka 0
-// Increment R3, Decriment R4
-// Check if equal... Continue
-// If they are ever not 0. Then they are not equal
-// Set R0 to 0
-// Exit infinitely to finish program
-//(Exit)
-//@Exit
-//0;JMP
-// If R4 and R3 have converged 
-// Or if the R3
-
-// Example 
-// @R3
-// D=MD
-// @R4
-// D=D-MD
-// @IS_PALINDROME
-// D;JGE // They have passed the comparison. Aka crossed
-
-// Increment number of characters read
-// Moved to (1)
-//@R2
-//M=M+1
-
-//R0 will be answer -1 for palindrom 0 for no palindrom
-//R1 will be temporary character read
-//R2 will be number of characters read
-//R3 Where to put the character just read
-//R4
-//R5 first character read
-//R6 second character read
